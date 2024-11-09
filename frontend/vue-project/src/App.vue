e78c03dca0c6c799029bff59b02b83ca03368d01
moh004 <moh.2004salehi@gmail.com>
2024-11-09 12:57:05 +0330

    adding readme index.js and ..

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,47 +1,23 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+      import { ref } from 'vue';
+      import axios from 'axios';
+
+    // Define a reactive variable to hold the response message
+    const message = ref('this is a msg');
+
+    // Define the function to call the backend
+    const fetchBackendData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000');
+        message.value = response.data; // Set the response data to the message variable
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+      <button @click="fetchBackendData" class="flex bg-sky-500 justify-center align-middle">Test Backend Connection</button>
+      <p>{{ message }}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
